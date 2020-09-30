dc2783b2dbc95f6e8faf87374dd4c1a645d4b7f9
azhar-azad <azhar.u.azad@gmail.com>
2020-10-01 00:46:47 +0600

    String Special Characters count done

--- src/com/azad/practice/java/string/MainString.java
+++ src/com/azad/practice/java/string/MainString.java
@@ -19,11 +19,37 @@ public class MainString {
         /*
         STRING 1: Count the total number of characters in a string
          */
-        stringDemo.init();
-
-        System.out.println("The length of \'" + stringDemo.getStr() + "\' is " + stringDemo.getStrLength());
+//        System.out.println("\n\nString Length Calculate\n============================\n");
+//
+//        stringDemo.init();
+//
+//        System.out.println("The length of \'" + stringDemo.getStr() + "\' is " + stringDemo.getStrLength());
         /*
         END STRING 1
+         */
+
+        /*
+        STRING 2: Count the total number of special characters in a string
+         */
+        System.out.println("\n\nCount Total Number of Special Characters in a String\n============================\n");
+
+        stringDemo.init();
+
+        System.out.println("Total number of special characters in \'" + stringDemo.getStr() + "\' is " + stringDemo.getSpecialCharacterCount());
+        /*
+        END STRING 2
+         */
+
+        /*
+        STRING 3: Count the number of constants and vowel in string
+         */
+//        System.out.println("\n\nCount Total Number of Constants and Vowels in a String\n============================\n");
+//
+//        stringDemo.init();
+//
+//        System.out.println("Total number of constants: " + stringDemo.getConstantsCount() + " and vowels: " + stringDemo.getVowelsCount());
+        /*
+        END STRING 3
          */
     }
 }
--- src/com/azad/practice/java/string/stringHelpers/StringDemo.java
+++ src/com/azad/practice/java/string/stringHelpers/StringDemo.java
@@ -33,4 +33,54 @@ public class StringDemo {
     public int getStrLength() {
         return str.length();
     }
+
+    public int getSpecialCharacterCount() {
+
+        int spCharCount = 0;
+
+        for (char ch: str.toCharArray())
+            spCharCount += isSpecialCharacter(ch) ? 1 : 0;
+
+        return spCharCount;
+    }
+
+    public int getVowelsCount() {
+
+        int vowelsCount = 0;
+
+        for (char ch: str.toLowerCase().toCharArray())
+            vowelsCount += isVowel(ch) ? 1 : 0;
+
+        return vowelsCount;
+    }
+
+    public int getConstantsCount() {
+
+        int constantsCount = 0;
+
+        for (char ch: str.toLowerCase().toCharArray())
+            constantsCount += isConstant(ch) ? 1 : 0;
+
+        return constantsCount;
+    }
+
+    private boolean isNumber(char ch) {
+        return ch >= 48 && ch <= 57;
+    }
+
+    private boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+
+    private boolean isConstant(char ch) {
+        return !isVowel(ch) && (ch >= 'a' && ch <= 'z');
+    }
+
+    private boolean isSpace(char ch) {
+        return ch == 32;
+    }
+
+    private boolean isSpecialCharacter(char ch) {
+        return !(isSpace(ch) || isNumber(ch) || isVowel(ch) || isConstant(ch));
+    }
 }
