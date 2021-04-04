6b380fa791fcaeff87933c089f603d64ebb5f56e
azhar-azad <azhar.u.azad@gmail.com>
2021-04-04 19:44:38 +0600

    JCR: 2, 3 done

---
+++ src/com/azad/practice/java/JavaCompleteRef/Jcr2Overview/SampleProgram1.java
@@ -0,0 +1,18 @@
+package com.azad.practice.java.JavaCompleteRef.Jcr2Overview;
+
+/*
+ * This is a simple Java program.
+ **/
+public class SampleProgram1 {
+
+    // Your program begins with a call to main().
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program.");
+    }
+}
+
+/**
+ * OUTPUT:
+ *
+ * This is a simple Java program.
+ * */
---
+++ src/com/azad/practice/java/JavaCompleteRef/Jcr3DataTypesVariablesArrays/Arrays/AutoArray.java
@@ -0,0 +1,15 @@
+package com.azad.practice.java.JavaCompleteRef.Jcr3DataTypesVariablesArrays.Arrays;
+
+// An improved version of the previous program i.e. Array.
+public class AutoArray {
+
+    public static void main(String[] args) {
+
+        int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        System.out.println("April has " + month_days[3] + " days.");
+    }
+}
+
+/**
+    April has 30 days.
+ * */
