23ecde1f727b72185516aaf9dd127e3a6947a0b4
azhar-azad <azhar.u.azad@gmail.com>
2021-04-04 13:21:24 +0600

    Main class edited

--- src/com/azad/practice/java/Main.java
+++ src/com/azad/practice/java/Main.java
@@ -1,45 +1,52 @@
 package com.azad.practice.java;
 
+import com.azad.practice.java.Array.MainArray;
+import com.azad.practice.java.Basic.MainBasic;
 import com.azad.practice.java.CustomGenericDS.MainCustomGenericDS;
 import com.azad.practice.java.FilesAndStreams.MainFilesAndStreams;
+import com.azad.practice.java.FunctionalInterfaces.MainFunctionalInterfaces;
 import com.azad.practice.java.Generic.MainGeneric;
 import com.azad.practice.java.GenericCollections.MainGenericCollections;
+import com.azad.practice.java.Lambda.MainLambda;
 import com.azad.practice.java.LambdasAndStreams.MainLambdasAndStreams;
+import com.azad.practice.java.Matrix.MainMatrix;
 import com.azad.practice.java.Recursion.MainRecursion;
+import com.azad.practice.java.Searching.MainSearching;
 import com.azad.practice.java.SearchingAndSorting.MainSearchingAndSorting;
+import com.azad.practice.java.String.MainString;
+import com.azad.practice.java.Thread.MainThread;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
-//        MainBasic.main(args);
+        MainBasic.main(args);
 
-//        MainArray.main(args);
+        MainArray.main(args);
 
-//        MainMatrix.main(args);
+        MainMatrix.main(args);
 
-//        MainString.main(args);
+        MainString.main(args);
          
-//        MainSearching.main(args);
+        MainSearching.main(args);
 
-//        MainLambda.main(args);
+        MainLambda.main(args);
 
-//        MainFunctionalInterfaces.main(args);
+        MainFunctionalInterfaces.main(args);
 
-//        MainThread.main(args);
+        MainThread.main(args);
 
-//        MainFilesAndStreams.main(args);
+        MainFilesAndStreams.main(args);
 
-//        MainGenericCollections.main(args);
+        MainGenericCollections.main(args);
 
-//        MainGeneric.main(args);
+        MainGeneric.main(args);
 
-//        MainCustomGenericDS.main(args);
+        MainCustomGenericDS.main(args);
 
-//        MainLambdasAndStreams.main(args);
+        MainLambdasAndStreams.main(args);
 
-//        MainRecursion.main(args);
+        MainRecursion.main(args);
 
         MainSearchingAndSorting.main(args);
     }
