dda978ed1161b9202a178d755b52b4e97b277226
azhar-azad <azhar.u.azad@gmail.com>
2021-04-06 00:48:12 +0600

    JCR: 5 done

---
+++ src/com/azad/practice/java/JavaCompleteRef/Jcr5ControlStatements/Jump/BreakLoop3.java
@@ -0,0 +1,26 @@
+package com.azad.practice.java.JavaCompleteRef.Jcr5ControlStatements.Jump;
+
+// Using break with nested loops.
+class BreakLoop3 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Pass " + i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j == 10)
+                    break; // terminate loop if j is 10
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Loop complete.");
+    }
+}
+
+/**
+     Pass 0: 0 1 2 3 4 5 6 7 8 9
+     Pass 1: 0 1 2 3 4 5 6 7 8 9
+     Pass 2: 0 1 2 3 4 5 6 7 8 9
+     Loop complete.
+ * */
---
+++ src/com/azad/practice/java/JavaCompleteRef/Jcr5ControlStatements/Selection/StringSwitch.java
@@ -0,0 +1,29 @@
+package com.azad.practice.java.JavaCompleteRef.Jcr5ControlStatements.Selection;
+
+// Use a string to control a switch statement.
+class StringSwitch {
+
+    public static void main(String[] args) {
+
+        String str = "two";
+
+        switch (str) {
+            case "one":
+                System.out.println("one");
+                break;
+            case "two":
+                System.out.println("two");
+                break;
+            case "three":
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("no match");
+                break;
+        }
+    }
+}
+
+/**
+     two
+ * */
