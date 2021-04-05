b246eaaa2921b1151399a28a1023020f00f290d9
azhar-azad <azhar.u.azad@gmail.com>
2021-04-05 23:33:15 +0600

    JCR:4 done

---
+++ src/com/azad/practice/java/JavaCompleteRef/Jcr4Operators/Bitwise/ByteShift.java
@@ -0,0 +1,23 @@
+package com.azad.practice.java.JavaCompleteRef.Jcr4Operators.Bitwise;
+
+// Left shifting a byte value.
+class ByteShift {
+
+    public static void main(String[] args) {
+
+        byte a = 64;
+        byte b;
+        int i;
+
+        i = a << 2;
+        b = (byte) (a << 2);
+
+        System.out.println("Original value of a: " + a);
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
+
+/**
+     Original value of a: 64
+     i and b: 256 0
+ * */
