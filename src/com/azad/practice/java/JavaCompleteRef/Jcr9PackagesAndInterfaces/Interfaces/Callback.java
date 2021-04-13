9715fe4b2446f95a6a63d0f81866b7d6cef3b781
azhar-azad <azhar.u.azad@gmail.com>
2021-04-14 00:34:24 +0600

    JCR 9 done

---
+++ src/com/azad/practice/java/JavaCompleteRef/Jcr9PackagesAndInterfaces/Interfaces/Callback.java
@@ -0,0 +1,5 @@
+package com.azad.practice.java.JavaCompleteRef.Jcr9PackagesAndInterfaces.Interfaces;
+
+interface Callback {
+    void callback(int param);
+}
---
+++ src/com/azad/practice/java/JavaCompleteRef/Jcr9PackagesAndInterfaces/Interfaces/TestIface2.java
@@ -0,0 +1,31 @@
+package com.azad.practice.java.JavaCompleteRef.Jcr9PackagesAndInterfaces.Interfaces;
+
+// Another implementation of callback
+class AnotherClient implements Callback {
+
+    // Implement Callback's interface
+    public void callback(int p) {
+        System.out.println("Another version of callback");
+        System.out.println("p squared is " + (p*p));
+    }
+}
+
+class TestIface2 {
+
+    public static void main(String[] args) {
+
+        Callback c = new Client();
+        AnotherClient ob = new AnotherClient();
+
+        c.callback(42);
+
+        c = ob; // c now refers to AnotherClient object
+        c.callback(42);
+    }
+}
+
+/**
+     callback called with 42
+     Another version of callback
+     p squared is 1764
+ * */
